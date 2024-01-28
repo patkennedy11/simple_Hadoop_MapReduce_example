9649c92cda13fb52447734b151aa75503327f037
Pat Kennedy <patkennedy1123@gmail.com>
2024-01-28 18:25:12 +0000

    added 15 more stopwords

--- mapper.py
+++ mapper.py
@@ -9,6 +9,12 @@ for line in sys.stdin:
     # split the line into words; splits on any whitespace
     words = line.split()
 
+    # make lower case
+    line =  line.lower()
+
+    # set stopwords and punctuation
+    stopwords = set (['the', 'and', 'a', 'for', 'to', 'I', 'be', 'or', '.', ',', ':', ';', '-', '?', '!'])
+
     # output tuples (word, 1) in tab-delimited format
     for word in words:
         print '%s\t%s' % (word, "1")
